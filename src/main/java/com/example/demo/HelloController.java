b4533882c26f823061f9abc75daddbd9eae10ab7
Shelby-R-J <srjoyner@purdue.edu>
2023-11-19 22:05:37 -0500

    Rebased to remove commit with sensitive data

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,12 +3,40 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Happy Birthday!";
+        Connection connection = DBConnection.getConnection();
+
+        String query = "SELECT *\n" +
+        "FROM users;";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            ResultSetMetaData metaData = rs.getMetaData();
+
+            String str = "";
+
+            while(rs.next()) {
+                int columnNum = metaData.getColumnCount();
+
+                for (int i = 1; i <= columnNum; i++) {
+                    str += rs.getString(i) + ", ";
+                }
+            }
+
+            return str;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "Happy Birthday!";
+        }
     }
 
 }
