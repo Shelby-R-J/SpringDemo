b7a600c71e148f814110d75939544a3a589cb91d
Shelby-R-J <srjoyner@purdue.edu>
2023-11-19 22:59:29 -0500

    Added secure password access through system environment variable

--- src/main/java/com/example/demo/DBConnection.java
+++ src/main/java/com/example/demo/DBConnection.java
@@ -9,17 +9,10 @@ import java.sql.SQLException;
 
 public class DBConnection {
     public static Connection getConnection() {
-
-        try {
-            FileReader fr = new FileReader("pass.txt");
-            BufferedReader bfr = new BufferedReader(fr);
-        } catch (FileNotFoundException e) {
-            System.out.println("Password file not detected.");
-        }
-
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://dpg-clcjgad4lnec73ckq67g-a.ohio-postgres.render.com:5432/testdb_4emv", "root", "pMeoYTqhagMAnRItmIe8LgNo0l30o74X");
+            String password = System.getenv("DB_PASSWORD");
+            connection = DriverManager.getConnection("jdbc:postgresql://dpg-clcjgad4lnec73ckq67g-a.ohio-postgres.render.com:5432/testdb_4emv", "root", password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
